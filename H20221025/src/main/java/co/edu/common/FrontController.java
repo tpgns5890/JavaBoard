bf3889f48a8f37a0740c69c834221de40bf02da8
sehun <tpgns5890@gmail.com>
2022-10-26 17:53:45 +0900

    1026

--- H20221025/src/main/java/co/edu/common/FrontController.java
+++ H20221025/src/main/java/co/edu/common/FrontController.java
@@ -1,5 +1,46 @@
 package co.edu.common;
 
-public class FrontController {
+import java.io.IOException;
+import java.util.HashMap;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.edu.control.MainControl;
+
+public class FrontController extends HttpServlet{
+	
+	HashMap<String, Control> controlList;
+	String charset = null;
+	
+	//init()
+//	@Override
+//	public void init() throws ServletException {
+//		ServletContext sc = this.getServletContext();
+//		sc.getInitParameter("charset");
+//	}
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		charset = config.getInitParameter("charset");
+		controlList = new HashMap<String, Control>();
+		
+		controlList.put("/main.do", new MainControl());
+	}
+	
+	
+	//service()
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI(); // http://localhost:8081/H20221025/main.do
+		String context = req.getContextPath();
+		String path = uri.substring(context.length());
+		
+		System.out.println(path);
+		Control subControl = controlList.get(path);
+		subControl.exec(req, resp); // main.do => control실행.
+	}
 }
---
+++ H20221025/src/main/java/co/edu/control/MainControl.java
@@ -0,0 +1,18 @@
+package co.edu.control;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.edu.common.Control;
+
+public class MainControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
